3e87b5883d52cae267876252e210a77848fb0429
Mohamed Ashraf <35531522+mohamaedashraf@users.noreply.github.com>
2020-07-03 18:06:11 +0200

    use mongoose
    instead of using mongodb directly we use mongoose

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose =require('mongoose')
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -20,25 +19,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5efc6b53fe4db71bad0d4657')
+  User.findById('5eff46d8d18bdbbe04af7283')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
+  
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-// mongoose.connect('mongodb+srv://NodeTest:rTgl4AD7IF0L3X6m@mongotest-ay262.mongodb.net/shop?retryWrites=true&w=majority')
-// .then(res=>{
-//   app.listen(3000);
-// })
-// .catch(err=>{
-//   console.log(err);
-// })
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://NodeTest:rTgl4AD7IF0L3X6m@mongotest-ay262.mongodb.net/shop?retryWrites=true&w=majority')
+.then(res=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user = new User({
+        name:'Mohamed',
+        email:'mohamed@gmail.com',
+        cart:{
+          items:[]
+        }
+    
+      });
+      user.save();
+    }
+  })
+
   app.listen(3000);
-});
+})
+.catch(err=>{
+  console.log(err);
+})
